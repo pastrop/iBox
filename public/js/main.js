246aae2d279131e4fca2066904e1bda5b4f7d187
pastrop <pas_trop2003@yahoo.com>
2015-08-03 11:16:08 -0400

    Baseline 1.0
    Fully operational system with the exception of the timestamping

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function(){
 
-//AJAX Call sends all the info about every product effectively seeding the DB, good for simulation NOT production 
+//AJAX Code Block for the store front
+//Sends all the info about every product effectively seeding the DB, good for simulation NOT production 
 
+//Code Block for the 1st Product - Store-Front Page
+$("#knopka_1").on('click', function(event) {
+  event.preventDefault();
+
+    var name_ = 'Rioja';
+    var producer_ = 'Luis Alegre';
+    var vintage_ = '2012';
+    var item = {'name': name_,
+                'producer': producer_,
+                'vintage' : vintage_};
+
+    $.ajax('/items', { //AJAX call code block
+        type: 'POST',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+    .done(function(items){
+      console.log(JSON.stringify(items));
+  })
+    .error(function(){console.log('doesnt fucking work!')});
+}); 
+//End of the AJAX call code block
+
+//Code Block for the 2nd Product - Store-Front Page
 $("#knopka_2").on('click', function(event) {
 	event.preventDefault();
 
@@ -35,12 +61,109 @@ $("#knopka_2").on('click', function(event) {
 }); 
 //End of the AJAX call code block
 
+//Code Block for the 3rd Product - Store-Front Page
+$("#knopka_3").on('click', function(event) {
+  event.preventDefault();
+
+    var name_ = 'Chablis';
+    var producer_ = 'Gilbert Pico';
+    var vintage_ = '2011';
+    var item = {'name': name_,
+                'producer': producer_,
+                'vintage' : vintage_};
+
+    $.ajax('/items', { //AJAX call code block
+        type: 'POST',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+    .done(function(items){
+      console.log(JSON.stringify(items));
+  })
+    .error(function(){console.log('doesnt fucking work!')});
+}); 
+//End of the AJAX call code block
+
+//Code Block for the 4th Product - Store-Front Page
+$("#knopka_4").on('click', function(event) {
+  event.preventDefault();
+
+    var name_ = 'Rose';
+    var producer_ = 'Cibonne';
+    var vintage_ = '2009';
+    var item = {'name': name_,
+                'producer': producer_,
+                'vintage' : vintage_};
+
+    $.ajax('/items', { //AJAX call code block
+        type: 'POST',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+    .done(function(items){
+      console.log(JSON.stringify(items));
+  })
+    .error(function(){console.log('doesnt fucking work!')});
+}); 
+//End of the AJAX call code block
+
+//Code Block for the 5th Product - Store-Front Page
+$("#knopka_5").on('click', function(event) {
+  event.preventDefault();
+
+    var name_ = 'Chardonnay';
+    var producer_ = 'Goisot';
+    var vintage_ = '2013';
+    var item = {'name': name_,
+                'producer': producer_,
+                'vintage' : vintage_};
+
+    $.ajax('/items', { //AJAX call code block
+        type: 'POST',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+    .done(function(items){
+      console.log(JSON.stringify(items));
+  })
+    .error(function(){console.log('doesnt fucking work!')});
+}); 
+//End of the AJAX call code block
+
+//Code Block for the 6th Product - Store-Front Page
+$("#knopka_6").on('click', function(event) {
+  event.preventDefault();
+
+    var name_ = 'Pinot Gris';
+    var producer_ = 'Reuilly';
+    var vintage_ = '2012';
+    var item = {'name': name_,
+                'producer': producer_,
+                'vintage' : vintage_};
+
+    $.ajax('/items', { //AJAX call code block
+        type: 'POST',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+    .done(function(items){
+      console.log(JSON.stringify(items));
+  })
+    .error(function(){console.log('doesnt fucking work!')});
+}); 
+//End of the AJAX call code block
+
 //Analytics AJAX calls
 
 $("#analyticsForm").on('submit', function(event) {
     event.preventDefault(); 
     var ID_ = $('#ID').val();
     console.log('var ID_: ',ID_);
+    // Only StoreID is currently used for the Analytics, the rest is placeholders
     var name_ = 'tinto roriz';
     var producer_ = 'santa fe';
     var vintage_ = '2010';
@@ -60,12 +183,23 @@ $("#analyticsForm").on('submit', function(event) {
     })
     .done(function(items){
       var totalcount=items.length;
+      var pr_1=0;
       var pr_2=0;
+      var pr_3=0;
+      var pr_4=0;
+      var pr_5=0;
+      var pr_6=0;
       for(i=0; i<totalcount; i++){
-        if(items[i].name == 'tinto roriz'){pr_2++}
+        //Jerry-rigged code to be refactored
+        if(items[i].name == 'Rioja'){pr_1++};
+        if(items[i].name == 'tinto roriz'){pr_2++};
+        if(items[i].name == 'Chablis'){pr_3++};
+        if(items[i].name == 'Rose'){pr_4++};
+        if(items[i].name == 'Chardonnay'){pr_5++};
+        if(items[i].name == 'Pinot Gris'){pr_6++};
       };  
       console.log(JSON.stringify(items));
-      console.log('total count: '+totalcount+' pr_2: '+pr_2);
+      console.log('total count: '+totalcount+' pr_1: '+pr_1);
 //Analytics Graphics
 
 var chartData={
@@ -74,11 +208,17 @@ var chartData={
             "values": ["Rioja", "Tinto Roriz", "Chablis", "Rose", "Chardonnay", "Pinot Gris"]
     },
   "series": [
-     {"values": [35, 0, 67, 89, 10, 5]} 
+     {"values": [0, 0, 0, 0, 0, 0]} 
 //    { "values": [28, 57, 43, 56] }
   ]
 };
+//Jerry-rigged code to be refactored
+chartData.series[0].values[0]=pr_1;
 chartData.series[0].values[1]=pr_2;
+chartData.series[0].values[2]=pr_3;
+chartData.series[0].values[3]=pr_4;
+chartData.series[0].values[4]=pr_5;
+chartData.series[0].values[5]=pr_6;
 console.log('checking chartData: '+chartData.series[0].values[0]);
 //window.onload=function(){
   zingchart.render({
@@ -89,11 +229,12 @@ console.log('checking chartData: '+chartData.series[0].values[0]);
   });
 //};
 //End of the Analytics Graphics
-
-
   })
     .error(function(){console.log('doesnt fucking work!')});
 }); //End of the AJAX code block
+
+
+
 
 /*//Analytics Graphics
 
